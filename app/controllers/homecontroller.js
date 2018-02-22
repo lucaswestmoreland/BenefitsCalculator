c92c0576b3b9569b5f37bb41272b3a992b3feb37
lucaswestmoreland <lucaswestmoreland@u.boisestate.edu>
2018-02-22 13:45:32 -0700

    Additional Staged Files

---
+++ app/controllers/homecontroller.js
@@ -0,0 +1,30 @@
+(function () { 
+'use strict';
+
+angular.module('app').controller('HomeController', HomeController)
+
+HomeController.$inject[]
+
+function HomeController()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
